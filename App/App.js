acc466878537d18abbe22b07f5989ae04711ff65
Novandi Kevin Pratama <nkpknight34@gmail.com>
2021-07-02 04:57:38 +0700

    change access cors

--- App/App.js
+++ App/App.js
@@ -12,7 +12,7 @@ app.use(function (req, res, next) {
   // Website you wish to allow to connect
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://webdokter.herokuapp.com/"
+    "https://webdokter.herokuapp.com"
   );
 
   // Request methods you wish to allow
