5d00860c1a21b10725d584ce3a518dba77aae5a0
Novandi Kevin Pratama <nkpknight34@gmail.com>
2021-07-03 14:36:02 +0700

    final

--- App/App.js
+++ App/App.js
@@ -65,43 +65,7 @@ app.use(Middleware);
 Route(app);
 
 app.get("/", (req, res) => {
-  var startTime = "10:00";
-  var endTime = "17:00";
-
-  var start_time = parseTime(startTime),
-    end_time = parseTime(endTime),
-    interval = 79;
-
-  var times_ara = calculate_time_slot(start_time, end_time, interval);
-
-  res.send({ waktu: times_ara });
+  res.send("Api dokter");
 });
-
-function parseTime(s) {
-  var c = s.split(":");
-  return parseInt(c[0]) * 60 + parseInt(c[1]);
-}
-
-function convertHours(mins) {
-  var hour = Math.floor(mins / 60);
-  var mins = mins % 60;
-  var converted = pad(hour, 2) + ":" + pad(mins, 2);
-  return converted;
-}
-
-function pad(str, max) {
-  str = str.toString();
-  return str.length < max ? pad("0" + str, max) : str;
-}
-
-function calculate_time_slot(start_time, end_time, interval = "30") {
-  var i, formatted_time;
-  var time_slots = new Array();
-  for (var i = start_time; i <= end_time; i = i + interval) {
-    formatted_time = convertHours(i);
-    time_slots.push(formatted_time);
-  }
-  return time_slots;
-}
 
 module.exports = app;
