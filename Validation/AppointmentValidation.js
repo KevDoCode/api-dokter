16fa87ef14fa4d68834c665615190935497a6e97
Novandi Kevin Pratama <nkpknight34@gmail.com>
2021-07-04 06:29:05 +0700

    time validation

--- Validation/AppointmentValidation.js
+++ Validation/AppointmentValidation.js
@@ -4,10 +4,23 @@ function validate() {
   return [
     body("iddoctor").custom(checkDoctor),
     body("starttime").matches("^([0-2][0-9]):[0-5][0-9]$"),
-    body("endtime").matches("^([0-2][0-9]):[0-5][0-9]$"),
+    body("endtime").matches("^([0-2][0-9]):[0-5][0-9]$").custom(checkTime),
     body("description").notEmpty(),
     body("duration").isNumeric(),
   ];
+}
+
+function checkTime(time,{req}){
+  let starttime = new Date("2021-04-04 "+req.body.starttime)
+  let endtime = new Date("2021-04-04 "+time)
+  return new Promise((resolve, reject) => {
+      if(starttime.getTime() > endtime.getTime()){
+          reject("End time must be more from start time");
+      }else{
+        resolve()
+      }
+  })
+  
 }
 
 async function checkDoctor(id) {
